b947f4c6523ddd4fa5faa63ad4d8894dce445f3c
zion379 <zspynet22@icloud.com>
2020-03-31 21:20:24 -0700

    Finished animation player animation setup.

--- Assets/Scripts/TestScripts/TestAnimationControls.cs
+++ Assets/Scripts/TestScripts/TestAnimationControls.cs
@@ -9,6 +9,8 @@ public class TestAnimationControls : MonoBehaviour
     public float speed = 10f;
     public float rotationSpeed = 100f;
 
+    public bool iscrouching = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -27,25 +29,42 @@ public class TestAnimationControls : MonoBehaviour
 
         if(Input.GetButtonDown("Jump"))
         {
-            
+            // check if player is moving
+            if (translation != 0 && !iscrouching)
+            {
+                // moving
+                anim.SetTrigger("RunJump");
+            }
+            else
+            {
+                // not moving
+                anim.SetTrigger("Jump");
+            }
         }
 
-        // check if player is moving
-        if (translation != 0)
+        if (Input.GetButtonDown("crouch"))
         {
-            
+            if (iscrouching)
+            {
+                iscrouching = false;
+            }
+            else
+            {
+                iscrouching = true;
+            }
+            anim.SetBool("Crouch", iscrouching);
         }
-        else
-        {
-            
-        }
+
+        
 
         float verticalAnimParmeter = Input.GetAxis("Vertical") * 10;
+        float HorizontalAnimParameter = Input.GetAxis("Horizontal") * 10;
 
-        Debug.Log($"translation : {verticalAnimParmeter}");
+        //Debug.Log($"translation : {verticalAnimParmeter}");
 
         // apply movement
-        anim.SetFloat("VelocityX", verticalAnimParmeter);
+        anim.SetFloat("VelY", verticalAnimParmeter);
+        anim.SetFloat("VelX", HorizontalAnimParameter);
 
     }
 }
